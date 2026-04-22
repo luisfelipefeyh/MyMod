a715c44d91adbe69060e6b2b613fd93c8e2d3c98
Luis Felipe de Azambuja Feyh <luisfelipe@feyh.com.br>
2026-04-22 10:42:07 -0300

    Add chisel advanced item

--- src/main/java/net/demolutio/tutorialmod/item/ModItems.java
+++ src/main/java/net/demolutio/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.demolutio.tutorialmod.item;
 
 import net.demolutio.tutorialmod.TutorialMod;
+import net.demolutio.tutorialmod.item.custom.ChiselItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,9 +14,12 @@ public class ModItems {
 
     public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite",
             () -> new Item(new Item.Properties()));
-    
+
     public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
             () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel",
+            () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
